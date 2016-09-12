cab64ff81211012be1ff12ea7037d14ca6c15930
omib2016 <omi1025@gmail.com>
2016-09-12 20:06:53 +0100

    Changed home page message.

--- consultant-db-webapp/src/main/java/com/undp/controller/UserController.java
+++ consultant-db-webapp/src/main/java/com/undp/controller/UserController.java
@@ -4,14 +4,14 @@ import com.undp.entity.User;
 import com.undp.service.AdminService;
 import com.undp.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Created by omib on 04/09/2016.
  */
-@RestController
+@Controller
 public class UserController
 {
     private final UserService userService;
@@ -24,30 +24,30 @@ public class UserController
         this.adminService = adminService;
     }
 
-    @RequestMapping("/getUser")
+    @GetMapping("/getUser")
     public User getUser(@RequestParam(value="name") String name)
     {
         return userService.getUserByName(name);
     }
 
-    @RequestMapping("/saveUser")
+    @PostMapping("/saveUser")
     public void saveUser(User user)
     {
         userService.saveUser(user);
     }
 
     //TODO: Move this function to its own controller.
-    @RequestMapping("/home")
-    public String getHomePage()
+    @RequestMapping("/")
+    public String home()
     {
-        return "UNICMS Home page.";
+        return "home";
     }
 
     @RequestMapping("/getStatus")
-    public String getStatus()
+    public String getStatus(Model model)
     {
         if (adminService.getSystemStatus() == 1)
-            return "UNICMS is up & running!";
+            return "status";
         else
             return "Error initializing database. Please check application logs..";
 
